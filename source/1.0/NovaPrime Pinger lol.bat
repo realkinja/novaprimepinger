b3955f2be6009cf0076b385f0b9082d8a3356bc7
Kinja <78376161+realkinja@users.noreply.github.com>
2021-05-02 22:56:49 +0200

    Cardi B

---
+++ source/1.0/NovaPrime Pinger lol.bat
@@ -0,0 +1,93 @@
+@echo off
+color 3
+title play.npbe.net 19132
+echo ==================================================
+echo                     NovaPrime
+echo Survive with limited botnets and rise to the top.
+echo       Start with entering your target's IP!
+echo                 Ctrl + C To stop
+echo ==================================================
+echo " .-._           _ __"    
+echo "/==/ \  .-._ .-`.' ,`. "
+echo "|==|, \/ /, /==/, -   \"
+echo "|==|-  \|  |==| _ .=. |"
+echo "|==| ,  | -|==| , '=',|" 
+echo "|==| -   _ |==|-  '..' "
+echo "|==|  /\ , |==|,  |"    
+echo "/==/, | |- /==/ - |"    
+echo "`--`./  `--`--`---'"                 
+:whosjoe
+set /p nicksacutie=Enter IP of your target:
+title Targeting %nicksacutie% In Warzone!
+echo ========================================
+echo Targeting "%nicksacutie%" In Warzone!
+echo ========================================
+:ad
+echo =======================================
+echo Play NovaPrime at play.npbe.net:19132
+echo =======================================
+:skiddy
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+PING -n 1 %nicksacutie% | FIND "TTL="
+IF ERRORLEVEL 1 goto lookatthismonkey
+cls
+goto ad
+:lookatthismonkey
+color 4
+title %nicksacutie% GET SLAMMED EZ
+echo    ___   ___          ___    _   _____     __    ___  ___   ___   __  ___ 
+echo   / __\ /___\/\/\    / __\  /_\ /__   \   / /   /___\/ _ \ / _ \ /__\/   \
+echo  / /   //  //    \  /__\// //_\\  / /\/  / /   //  // /_\// /_\//_\ / /\ /
+echo / /___/ \_// /\/\ \/ \/  \/  _  \/ /    / /___/ \_// /_\\/ /_\\//__/ /_// 
+echo \____/\___/\/    \/\_____/\_/ \_/\/     \____/\___/\____/\____/\__/___,'  
+color 3
+goto ad
